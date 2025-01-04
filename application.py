1ea80b6905d53fcb18cd618d9879a00b66477b2d
scorve12 <scorve5322@gmail.com>
2025-01-05 03:55:46 +0900

    FIX: application 수정정

--- application.py
+++ application.py
@@ -1,20 +1,5 @@
-import os
-from django.core.wsgi import get_wsgi_application
-import logging
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-application = get_wsgi_application()
-
-logger = logging.getLogger(__name__)
-
-def application_with_content_length_handling(environ, start_response):
+def application(environ, start_response):
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
-    except:
+    except (ValueError):
         request_body_size = 0
-    
-    logger.info(f"Request body size: {request_body_size}")
-    
-    # 기본 WSGI 애플리케이션 호출
-    return application(environ, start_response)
