3a2517f08da24c3e07543ae1837d934df173675b
scorve12 <scorve5322@gmail.com>
2025-01-06 00:54:32 +0900

    FEAT: AWS Nginx설정

--- application.py
+++
@@ -1,5 +0,0 @@
-def application(environ, start_response):
-    try:
-        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
-    except (ValueError):
-        request_body_size = 0
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# 애플리케이션 객체 노출
 application = get_wsgi_application()
