549efc66f1322aaa38791c24089159476b6a700d
scorve12 <scorve5322@gmail.com>
2025-01-07 20:09:04 +0900

    FEAT: application 추가가

--- core/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-# 애플리케이션 객체 노출
-application = get_wsgi_application()
