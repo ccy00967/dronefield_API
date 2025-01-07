5f09d206912995da0566babce89b728321ddaf59
scorve12 <scorve5322@gmail.com>
2025-01-08 01:21:23 +0900

    FIX: pkgutil

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import pkgutil
+import pkg_resources
+
+if not hasattr(pkgutil, 'ImpImporter'):
+    pkgutil.ImpImporter = pkgutil.zipimporter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
