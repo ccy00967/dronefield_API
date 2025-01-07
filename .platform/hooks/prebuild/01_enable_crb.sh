4379aa8b30e909b5ad0fcbe41ee0b2b692a1a84e
scorve12 <scorve5322@gmail.com>
2025-01-08 05:44:58 +0900

    FIX: enable 수정정

--- .platform/hooks/prebuild/01_enable_crb.sh
+++ .platform/hooks/prebuild/01_enable_crb.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-# 꼭 chmod +x 해주세요 (실행 권한 필수)
-
-echo "===== Enabling CRB on Amazon Linux 2023 ====="
-yum config-manager --set-enabled crb || true
-yum clean all
-yum update -y
-
-echo "===== CRB enabled successfully ====="
+echo "===== Enabling codeready repo on AL2023 ====="
+yum config-manager --set-enabled amazon-linux-2023-codeready
+yum clean all && yum update -y
