70ca916a6099ec923cd4f06461ba68753b4d7101
Chae Chulyoung <ccy09671324@gmail.com>
2025-01-08 17:27:12 +0900

    FIX: nicepass

--- user/views.py
+++ user/views.py
@@ -298,7 +298,7 @@ def niceCallback(request):
         request.session["name"] = dec_data.get("name")
         request.session["birthdate"] = dec_data.get("birthdate")
         request.session["gender"] = dec_data.get("gender")
-        request.session["nationalinfo"] = dec_data("nationalinfo")
+        request.session["nationalinfo"] = dec_data.get("nationalinfo")
         request.session["mobileno"] = dec_data.get("mobileno")
 
         request.session[isNicePassDone] = True
@@ -310,6 +310,7 @@ def niceCallback(request):
                 "name": dec_data.get("name"),
                 "birthdate": dec_data.get("birthdate"),
                 "gender": dec_data.get("gender"),
+                "nationalinfo": dec_data.get("nationalinfo"),
                 "mobileno": dec_data.get("mobileno"),
             },
             status=200,
